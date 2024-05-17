b81b1e1f2ae9a48222cf1d4b702519c816a34160
muhammedola <141998553+muhammedola@users.noreply.github.com>
2024-05-17 10:01:42 +0100

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import {
+	Navbar,
+	Footer,
+	Welcome,
+	Services,
+	Transactions,
+	Loader,
+} from "@/components";
+
+export default function Page() {
+	return (
+		<div className="min-h-screen">
+			<div className="gradient-bg-welcome">
+				<Navbar />
+				<Welcome/>
+			</div>
+		</div>
+	);
+}
